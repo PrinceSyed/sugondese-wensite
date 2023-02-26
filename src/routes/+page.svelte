f0aeaefd3ab9ffc95c72d6490cc3ac1e7e79c939
Spike <syedibrahim1388@gmail.com>
2023-02-26 02:50:40 -0500

    video update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <div class="hero min-h-screen">
   <div class="sugon-video-container">
-    <video autoplay loop class="sugon-video">
+    <video class="sugon-video" autoplay loop>
       <source src="/images/sugon-demo.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
     </video>
   </div>
 
