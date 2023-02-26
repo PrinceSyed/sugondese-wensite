eb2fab86942a7344aee2d3b821f4be901a6f871a
Spike <syedibrahim1388@gmail.com>
2023-02-26 02:40:48 -0500

    added video

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 <!-- Navbar -->
 
 
-<div class="navbar bg-base-100">
+<div class="navbar font-medium">
 	<div class="container">
 		<div class="flex-1">
 			<img src="/images/sugon-icon.png" alt="Sugondese Ninjas Game Logo"/>
@@ -15,7 +15,7 @@
 				<li><a href="#collectibles"> Collectibles </a></li>
 
 				<li><a href="#lore"> Lore </a></li>
-				<a href="#lore">
+				<a href="#lore" class="ml-5">
 					<button class="btn btn-primary"> Play Now </button>
 				</a>
 			</ul>
@@ -24,3 +24,10 @@
 </div>
 
 <slot />
+
+<style>
+.navbar {
+    background: #ffffff54;
+    backdrop-filter: blur(10px);
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
-<div
-  class="hero min-h-screen"
-  style="background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
->
-  <div class="hero-overlay bg-opacity-60" />
-  <div class="hero-content text-center text-neutral-content">
+<div class="hero min-h-screen">
+  <div class="sugon-video-container">
+    <video autoplay loop class="sugon-video">
+      <source src="/images/sugon-demo.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+
+  <div />
+
+  <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="mb-5 text-5xl font-bold">Welcome to Sugondesee</h1>
       <p class="mb-5">
         Mini game where you try to get these little ninjas out of a gambling
         debt.
       </p>
-      <button class="btn btn-primary"> Play Now </button>
+      <button class="btn btn-primary w-full"> Play Now </button>
     </div>
   </div>
 </div>
@@ -63,3 +68,21 @@
     <!-- End Card 1 -->
   </div>
 </div>
+
+<style>
+  .sugon-video {
+    position: absolute;
+    top: -20px;
+    left: 0px;
+    width: 100%;
+    z-index: -1;
+  }
+
+  .hero-content {
+    background: rgba(239, 204, 255, 0.75);
+    backdrop-filter: blur(10px);
+    padding: 30px 30px;
+    border-radius: 10px;
+    margin: -60px 0px 0px 0px;
+  }
+</style>
