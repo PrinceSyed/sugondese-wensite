c336e2a74a26eed7f71cf449b7fef0125aaceeee
Spike <syedibrahim1388@gmail.com>
2023-02-26 17:30:25 -0500

    collectibles update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,19 +5,19 @@
 <!-- Navbar -->
 
 
-<div class="navbar font-medium">
+<div class="navbar font-medium text-base-100">
 	<div class="container">
 		<div class="flex-1">
-			<img src="/images/sugon-icon.png" alt="Sugondese Ninjas Game Logo"/>
+			<img src="/images/sugon-icon.png" alt="Sugondese Ninjas Game Logo" class="nav-img"/>
 		</div>
 		<div class="flex-none">
 			<ul class="menu menu-horizontal px-1">
 				<li><a href="#collectibles"> Collectibles </a></li>
 
 				<li><a href="#lore"> Lore </a></li>
-				<a href="#lore" class="ml-5">
+				<!-- <a href="#lore" class="ml-5">
 					<button class="btn btn-primary"> Play Now </button>
-				</a>
+				</a> -->
 			</ul>
 		</div>
 	</div>
@@ -26,8 +26,13 @@
 <slot />
 
 <style>
+
 .navbar {
-    background: #ffffff54;
-    backdrop-filter: blur(10px);
+    backdrop-filter: blur(5px);
+	background: #000c;
 }
+.nav-img {
+	max-width: 50px;
+}
+
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="hero min-h-screen">
   <div class="sugon-video-container">
-    <video class="sugon-video" autoplay muted playsinline loop> 
+    <video class="sugon-video" autoplay muted playsinline loop>
       <source src="/images/sugon-demo-1.mp4" type="video/mp4" />
       <source src="/images/sugon-demo-1.webm" type="video/webm" />
     </video>
@@ -8,30 +8,45 @@
 
   <div />
 
-  <div class="hero-content text-center">
+  <div class="hero-content main-hero text-center">
     <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">Welcome to Sugondesee</h1>
+      <h1 class="mb-5 text-5xl font-bold">Welcome to Sugondese</h1>
       <p class="mb-5">
         Mini game where you try to get these little ninjas out of a gambling
-        debt.
+        debt by collecting shitcoins.
       </p>
-      <button class="btn btn-primary w-full"> Play Now </button>
+      <a href="https://discord.gg/K6Qz93YZEd" target="_blank" rel="noreferrer">
+        <button class="btn btn-primary w-full"> Join DIscord For Updates </button>
+      </a> 
+      <p class="text-sm mt-2"> Coming Soon on Windows and Macs </p>
     </div>
   </div>
 </div>
 
-<div id="collectibles" class="mt-20">
+<!-- Collectibles Title -->
+<div id="collectibles" class="mt-10 mb-10 pb-20 pt-5">
+  <div class="container text-center">
+    <div class="max-w-md mx-auto mb-10">
+      <h3 class="font-bold text-3xl">Game Ready Collectibes</h3>
+      <p class="pt-2">
+        This game was made with collectibles integration in mind from day one.
+        The game is free to play for everyone but these skins will give you
+        access to leaderboard.
+      </p>
+    </div>
+  </div>
+
   <div class="collectibles container flex gap-x-6 gap-y-4 max-sm:flex-col">
     <!-- Card 1 -->
     <div class="card card-compact bg-base-100 shadow-xl">
       <figure>
-        <img src="/images/zach.png" alt="Sugon Collectible 1" />
+        <img src="/images/collectible-image-1.png" alt="Sugon Collectible 1" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Landon</h2>
-        <p>Landon</p>
+        <h2 class="card-title"> Ninja 1</h2>
+        <p> Digital collectible coming soon </p>
         <div class="card-actions justify-center">
-          <button class="btn btn-primary w-full"> View Collectible </button>
+          <!-- <button class="btn btn-primary w-full"> View Collectible </button> -->
         </div>
       </div>
     </div>
@@ -40,13 +55,13 @@
     <!-- Card 1 -->
     <div class="card card-compact bg-base-100 shadow-xl">
       <figure>
-        <img src="/images/zach.png" alt="Sugon Collectible 1" />
+        <img src="/images/collectible-image-2.png" alt="Sugon Collectible 2" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Landon</h2>
-        <p>Landon</p>
+        <h2 class="card-title"> Ninja 2</h2>
+        <p> Digital collectible coming soon </p>
         <div class="card-actions justify-center">
-          <button class="btn btn-primary w-full"> View Collectible </button>
+          <!-- <button class="btn btn-primary w-full"> View Collectible </button> -->
         </div>
       </div>
     </div>
@@ -55,13 +70,13 @@
     <!-- Card 1 -->
     <div class="card card-compact bg-base-100 shadow-xl">
       <figure>
-        <img src="/images/zach.png" alt="Sugon Collectible 1" />
+        <img src="/images/collectible-image-1.png" alt="Sugon Collectible 3" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Landon</h2>
-        <p>Landon</p>
+        <h2 class="card-title"> Ninja 3</h2>
+        <p> Digital collectible coming soon </p>
         <div class="card-actions justify-center">
-          <button class="btn btn-primary w-full"> View Collectible </button>
+          <!-- <button class="btn btn-primary w-full"> View Collectible </button> -->
         </div>
       </div>
     </div>
@@ -69,6 +84,29 @@
   </div>
 </div>
 
+<!-- Start Lore -->
+
+<div id="lore">
+  <div class="hero min-h-screen bg-base-content text-base-100">
+    <div class="container">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src="/images/zach.png" alt="Sugon Collectible 1" />
+        <div>
+          <h3 class="font-bold text-3xl">Lore</h3>
+          <p class="py-6">
+            These Ninjas are from the land of Sugondese. They are kind hearted Ninjas but they have a gambing addiction which caused them to lose their Dojo to the evil rich billionaird Mr Landon. 
+          </p>
+          <p>
+            Ever since then, they have been participating in Game Shows hosted by the elite, putting their bodies on line just to make some shitcoin which hopefully they can use to buy back their dojo. If the shitcoin devs don’t rug of course. 
+          </p>
+       
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Start Styles -->
 <style>
   .sugon-video {
     position: absolute;
@@ -78,7 +116,7 @@
     z-index: -1;
   }
 
-  .hero-content {
+  .hero-content.main-hero  {
     background: rgba(239, 204, 255, 0.75);
     backdrop-filter: blur(10px);
     padding: 30px 30px;
