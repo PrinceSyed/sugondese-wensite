3aeb739a0611459f5f74124af5f0c2a1ccb29fd0
Spike <syedibrahim1388@gmail.com>
2023-02-25 23:05:37 -0500

    added collectible section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,65 @@
-<div class="hero min-h-screen" style="background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
-    <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content">
-      <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold"> Welcome to Sugondesee </h1>
-        <p class="mb-5">
-            Mini game where you try to get these little ninjas out of a gambling debt.
-        </p>
-        <button class="btn btn-primary"> Play Now </button>
-      </div>
+<div
+  class="hero min-h-screen"
+  style="background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+>
+  <div class="hero-overlay bg-opacity-60" />
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Welcome to Sugondesee</h1>
+      <p class="mb-5">
+        Mini game where you try to get these little ninjas out of a gambling
+        debt.
+      </p>
+      <button class="btn btn-primary"> Play Now </button>
     </div>
   </div>
+</div>
+
+<div id="collectibles" class="mt-20">
+  <div class="collectibles container flex gap-x-6 gap-y-4 max-sm:flex-col">
+    <!-- Card 1 -->
+    <div class="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img src="../src/static/zach.png" alt="Sugon Collectible 1" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Landon</h2>
+        <p>Landon</p>
+        <div class="card-actions justify-center">
+          <button class="btn btn-primary w-full"> View Collectible </button>
+        </div>
+      </div>
+    </div>
+    <!-- End Card 1 -->
+
+    <!-- Card 1 -->
+    <div class="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img src="../src/static/zach.png" alt="Sugon Collectible 1" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Landon</h2>
+        <p>Landon</p>
+        <div class="card-actions justify-center">
+          <button class="btn btn-primary w-full"> View Collectible </button>
+        </div>
+      </div>
+    </div>
+    <!-- End Card 1 -->
+
+    <!-- Card 1 -->
+    <div class="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img src="../src/static/zach.png" alt="Sugon Collectible 1" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Landon</h2>
+        <p>Landon</p>
+        <div class="card-actions justify-center">
+          <button class="btn btn-primary w-full"> View Collectible </button>
+        </div>
+      </div>
+    </div>
+    <!-- End Card 1 -->
+  </div>
+</div>
