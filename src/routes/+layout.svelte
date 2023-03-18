71538d81323675b2bf8de8e543392f999288ed5f
Spike <syedibrahim1388@gmail.com>
2023-03-18 13:47:27 -0400

    g

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
 	import "../app.css";
 </script>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HXBCLCTL1E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HXBCLCTL1E');
+</script>
+
 <!-- Navbar -->
 
 
